502e54b78ecf8b1eb7138b0c5edb559d98e8bf10
Grecia Delgado <greciadelgado@Grecias-MacBook-Pro.local>
2025-01-17 14:32:52 -0500

    Add new items into table products

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe} from '@nestjs/common'
+import { Logger, ValidationPipe} from '@nestjs/common'
 import { envs } from './config';
 
 
 async function bootstrap() {
+
+  const logger = new Logger('main');
   const app = await NestFactory.create(AppModule);
  
  app.useGlobalPipes(
@@ -15,6 +17,6 @@ async function bootstrap() {
  )
 
   await app.listen(envs.port);
-  console.log(`Application is running on: ${envs.port}`);
+  logger.log(`Application is running on: ${envs.port}`);
 }
 bootstrap();
