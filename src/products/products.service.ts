f4470b706c87228f93ca4f316d2eaab332148b3a
Grecia Delgado <greciadm2596@gmail.com>
2025-01-24 18:46:20 -0500

    Add index tinto prisma

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -34,6 +34,7 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
       data: await this.product.findMany({
         skip: (page - 1) * limit,
         take: limit,
+        where: { available: true },
       }),
       metadata: {
         total: totalPages,
