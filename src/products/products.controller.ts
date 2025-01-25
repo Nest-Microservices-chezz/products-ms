76890b6c55cb7acce6bf87b1cf1e403320cd6991
Grecia Delgado <greciadm2596@gmail.com>
2025-01-24 21:01:25 -0500

    Update product controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,34 +1,53 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query, ParseIntPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  @Post()
-  create(@Body() createProductDto: CreateProductDto) {
+  // @Post() // si puede estar , pero como ahora no es un hibrido se comenta
+  @MessagePattern({ cmd: 'create_product' })
+  create(@Payload() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
-  @Get()
+  // @Get()
+  @MessagePattern({ cmd: 'find_products' })
   findAll(@Query() paginationDto: PaginationDto) {
     return this.productsService.findAll(paginationDto);
   }
 
-  @Get(':id')
+  // @Get(':id')
+  @MessagePattern({ cmd: 'find_product' })
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id', ParseIntPipe) id: string, @Body() updateProductDto: UpdateProductDto) {
+  //@Patch(':id')
+  @MessagePattern({ cmd: 'update_product' })
+  update(
+    @Param('id', ParseIntPipe) id: string,
+    @Payload() updateProductDto: UpdateProductDto,
+  ) {
     return this.productsService.update(+id, updateProductDto);
   }
 
-  @Delete(':id')
+  //@Delete(':id')
+  @MessagePattern({ cmd: 'delete_product' })
   remove(@Param('id') id: string) {
     return this.productsService.remove(+id);
   }
