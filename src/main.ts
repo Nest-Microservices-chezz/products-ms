f42d918e49d79f31061ca7d2c5be8bff4185d943
Grecia Delgado <greciadm2596@gmail.com>
2025-01-24 20:52:49 -0500

    change transport to HTTP to TCP

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger, ValidationPipe} from '@nestjs/common'
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
-
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-
   const logger = new Logger('main');
-  const app = await NestFactory.create(AppModule);
- 
- app.useGlobalPipes(
-  new ValidationPipe({
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
-  })
- )
+    }),
+  );
 
-  await app.listen(envs.port);
-  logger.log(`Application is running on: ${envs.port}`);
+  await app.listen();
+  logger.log(`Products microservices is running on: ${envs.port}`);
 }
 bootstrap();
